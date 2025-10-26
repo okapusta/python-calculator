ffe1df190825467d65930f51f0309e47a51b2af5
Oskar Kapusta <kapuszasty@gmail.com>
2025-10-26 16:22:40 +0100

    Fix linter errors

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-# save this as app.py
+""""
+This is a calculator webservice
+"""
+
 from flask import Flask, jsonify, request
 from calculator import Calculator
 
@@ -6,30 +9,42 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    """"
+    Healthcheck
+    """
+
     return "Hello, World!"
 
 @app.route("/calculate")
 def calculate():
+    """"
+    Calculator API
+    """
+
     op   = request.args.get('op', type=str)
     arg1 = request.args.get('arg1', type=int)
     arg2 = request.args.get('arg2', type=int)
 
+    if not op:
+        return jsonify({ "error": "missing operation" })
+    if not arg1 or not arg2:
+        return jsonify({ "error": "missing numbers" })
+
+    return jsonify({ "result": __run_calculations(op, arg1, arg2 )})
+
+def __run_calculations(operation, arg1, arg2):
     calculator = Calculator(arg1, arg2)
 
-    if not op:
-      return jsonify({ "error": "missing operation" })
-    if not arg1 or not arg2:
+    if operation == 'sum':
+        return calculator.sum()
+    if operation == 'subtract':
+        return calculator.subtract()
+    if operation == 'multiply':
+        return calculator.multiply()
+    if operation == 'divide':
+        return calculator.divide()
 
-      return jsonify({ "error": "missing numbers" })
-
-    if op == 'sum':
-        return jsonify({ "result": calculator.sum() })
-    if op == 'subtract':
-        return jsonify({ "result": calculator.subtract() })
-    if op == 'multiply':
-        return jsonify({ "result": calculator.multiply() })
-    if op == 'divide':
-        return jsonify({ "result": calculator.divide() })
+    return None
 
 if __name__ == '__main__':
     app.run(debug=True)
